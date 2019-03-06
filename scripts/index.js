03e1ac6f27d71249cf6f275f5693edf25810b06d
Ashish Garg <mragarg@gmail.com>
2019-03-06 16:03:17 -0500

    enlarges image on click. click image again and it disappears

--- scripts/index.js
+++ scripts/index.js
@@ -2,14 +2,27 @@ console.log("Script Link HTML test");
 
 // Variable that refers to all the pictures (data-picture)
 const targetPictures = document.querySelectorAll("[data-picture]");
+const fullViewPicture = document.querySelector("[data-fullView]");
 
+let clickPic = ""
 // Function for Click Response
 function respondToClick(event) {
     console.log("Click Function is working");
     console.log(event.target);
+    if(clickPic == ""){
+        clickPic = event.target.src;
+        fullViewPicture.src = clickPic;
+    }
+    else {
+        clickPic = "";
+        fullViewPicture.src = clickPic;
+    }
 }
 
 function attachClickHandler(eachElement){
     eachElement.addEventListener("click", respondToClick);
+    clickPic = eachElement.src;
 }
+
 targetPictures.forEach(attachClickHandler);
+
